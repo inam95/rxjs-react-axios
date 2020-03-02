4358e996d9a4af8bf53065869dd398935d62f207
Inamul Hassan <mfihassan99@gmail.com>
2020-03-02 10:03:52 +0530

    first steps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Observable, from } from 'rxjs';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    users: []
+  };
+  componentDidMount() {
+    // this.fetchUsers();
+    // this.fetchUsersObservables();
+    // this.fetchUsersFrom();
+  }
+
+  fetchUsers = async () => {
+    const promise = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    const data = promise.data;
+    this.setState({ users: data });
+  };
+
+  // fetch users by converting promise into an observable manually
+  // Writing a custom observer to handle the success response, error response and cancel request
+  fetchUsersObservables = () => {
+    const observable = new Observable(observer => {
+      axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+          observer.next(res);
+          observer.complete();
+        })
+        .catch(err => observer.error(err));
+    });
+
+    observable.subscribe(res => {
+      this.setState({
+        users: res.data
+      });
+    });
+  };
+
+  // fetch users by already build "from" function in rxjs library
+  // Advantage is that you don't have to write any custom observer, handling the success response, error responses and cancel request are all defined correctly for you
+  fetchUsersFrom = () => {
+    const promise = axios.get('https://jsonplaceholder.typicode.com/users');
+    const observable = from(promise);
+
+    observable.subscribe(res => {
+      this.setState({
+        users: res.data
+      });
+    });
+  };
+
+  render() {
+    const { users } = this.state;
+    return (
+      <div className="App">
+        {users.map(u => (
+          <ul key={u.id}>
+            <li>{u.name}</li>
+          </ul>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
